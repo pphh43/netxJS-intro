84f2d405cc00d3b00596e4750ebe22afd25a22f1
Dorothy <dorothy@ntcon.co>
2022-04-01 14:45:48 +0900

    1.3 여러개 module style 적용 방법 두가지

--- components/NavBar.js
+++ components/NavBar.js
@@ -1,17 +1,32 @@
 import Link from "next/link";
 import { useRouter } from "next/router";
+import styles from "./NavBar.module.css";
 
 export default function NavBar() {
+  const router = useRouter();
+  console.log(router);
 
-    const router = useRouter();
-    console.log(router);
-
-    return <nav>
-        <Link href="/">
-            <a style={{color : router.pathname ==="/" ? "black" : "red"}}>Home</a>
-        </Link>
-        <Link href="/about">
-            <a style={{color : router.pathname ==="/about" ? "black" : "red"}}>about</a>
-        </Link>
+  return (
+    <nav className={styles.nav}>
+      <Link href="/">
+        <a
+          className={`${styles.link} ${
+            router.pathname === "/" ? styles.active : ""
+          }`} // 여러개 module style 적용 방법 1 : className={`${styles.name} ${styles.name}`}
+        >
+          Home
+        </a>
+      </Link>
+      <Link href="/about">
+        <a
+          className={[
+            styles.link,
+            router.pathname === "/about" ? styles.active : "",
+          ].join(" ")} // 여러개 module style 적용 방법 2 : {[styles.name, styles.name].join("")}
+        >
+          about
+        </a>
+      </Link>
     </nav>
+  );
 }
